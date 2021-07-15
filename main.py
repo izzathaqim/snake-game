d7bfd21cfeb212d8cd31fe48d3aa0166122dd5df
izzat <izzathaqim@gmail.com>
2021-07-15 23:34:38 +0800

    add control function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import turtle
 import time
 import snake
 
+
 x_position = 0
 game_is_on = True
 
@@ -12,10 +13,11 @@ screen.title("Snake Games")
 screen.tracer(0)
 
 snake = snake.Snake()
+snake.control()
 
 while game_is_on:
     screen.update()
-    time.sleep(0.5)
+    time.sleep(0.1)
     snake.move(20)
 
 
--- snake.py
+++ snake.py
@@ -2,13 +2,16 @@ import turtle
 
 # Global Variables
 POSITION = [(0, 0), (-20, 0), (-40, 0)]
-MOVE_DISTANCE = 20
 
 
 class Snake:
     def __init__(self):
         self.snakes = []
         self.create_snake()
+        self.move_up()
+        self.move_down()
+        self.move_left()
+        self.move_right()
 
     def create_snake(self):
         for position in POSITION:
@@ -24,3 +27,27 @@ class Snake:
             new_y = self.snakes[snake_number - 1].ycor()
             self.snakes[snake_number].goto(x=new_x, y=new_y)
         self.snakes[0].forward(MOVE_DISTANCE)
+
+    def control(self):
+        screen = turtle.Screen()
+        screen.listen()
+        screen.onkey(key="Up", fun=self.move_up)
+        screen.onkey(key="Down", fun=self.move_down)
+        screen.onkey(key="Left", fun=self.move_left)
+        screen.onkey(key="Right", fun=self.move_right)
+
+    def move_up(self):
+        self.snakes[0].setheading(90)
+        self.snakes[0].forward(20)
+
+    def move_down(self):
+        self.snakes[0].setheading(270)
+        self.snakes[0].forward(20)
+
+    def move_right(self):
+        self.snakes[0].setheading(0)
+        self.snakes[0].forward(20)
+
+    def move_left(self):
+        self.snakes[0].setheading(180)
+        self.snakes[0].forward(20)
