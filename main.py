310cbdb0e51f9349357f5576eeba5e5e25d02b52
izzat <izzathaqim@gmail.com>
2021-07-15 23:15:38 +0800

    add snake class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import turtle
 import time
+import snake
 
 x_position = 0
 game_is_on = True
@@ -10,24 +11,14 @@ screen.bgcolor("black")
 screen.title("Snake Games")
 screen.tracer(0)
 
-snakes = [turtle.Turtle(shape="square") for x in range(3)]
-positions = [(0, 0), (-20, 0), (-40, 0)]
-
-for position in positions:
-    location = positions.index(position)
-    snakes[location].goto(position)
-    snakes[location].color("white")
-    snakes[location].penup()
+snake = snake.Snake()
 
 while game_is_on:
     screen.update()
     time.sleep(0.5)
+    snake.move(20)
 
-    for snake_number in range(len(snakes) - 1, 0, -1):
-        new_x = snakes[snake_number - 1].xcor()
-        new_y = snakes[snake_number - 1].ycor()
-        snakes[snake_number].goto(x=new_x, y=new_y)
-    snakes[0].forward(20)
+
 
 
 
